ff4f11eb80a11450f5408cd47cbbd35511a62858
bzanetis <britt.zanetis19@gmail.com>
2018-07-05 14:19:45 -0500

    Assignment 8 completed

--- src/App.js
+++ src/App.js
@@ -8,13 +8,21 @@ class App extends Component {
     super(props);
     this.state = {
       todos: [
-        { description: 'Walk the cat', isCompleted: true },
-        { description: 'Throw the dishes away', isCompleted: false },
-        { description: 'Buy new dishes', isCompleted: false }
+        { description: 'Walk the cat', isCompleted: true, isDeleted: true},
+        { description: 'Throw the dishes away', isCompleted: false, isDeleted: false},
+        { description: 'Buy new dishes', isCompleted: false, isDeleted: false}
       ],
       newTodoDescription: ''
     };
  }
+
+ deleteTodo(index) {
+      const todos = this.state.todos.filter((todo, i) => i !== index);
+    //  const todo = todos[index];
+    //  todo.isDeleted = todo.isDeleted ? false : true;
+      this.setState({ todos: todos });
+   }
+
 
  handleChange(e) {
      this.setState({ newTodoDescription: e.target.value })
@@ -39,13 +47,19 @@ toggleComplete(index) {
       <div className="App">
         <ul>
           { this.state.todos.map( (todo, index) =>
-           <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) } />
+           <ToDo key={ index }
+           description={ todo.description }
+           isCompleted={ todo.isCompleted }
+           toggleComplete={ () => this.toggleComplete(index) }
+           deleteTodo={ () => this.deleteTodo(index)}
+           />
          )}
         </ul>
         <form onSubmit={ (e) => this.handleSubmit(e) }>
           <input type="text" value={ this.state.newTodoDescription } onChange={ (e) => this.handleChange(e) } />
           <input type="submit" />
         </form>
+
       </div>
     );
   }
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -2,11 +2,12 @@
 
  class ToDo extends Component {
    render() {
-     return (
-       <li>
-         <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
-         <span>{ this.props.description }</span>
-       </li>
+    return (
+        <li>
+          <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
+          <span>{ this.props.description }</span>
+          <input type="button" value="Delete" onClick={ this.props.deleteTodo } />
+        </li>
      );
    }
  }
